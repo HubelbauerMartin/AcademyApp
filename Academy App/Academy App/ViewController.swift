0fb0ec471a24bbf1973ab675b0137ae85256d606
Martin Hübelbauer <martin.hubelbauer@outlook.com>
2019-03-25 19:15:00 +0100

    Add light status bar in mail storyboard

--- Academy App/Academy App/ViewController.swift
+++ Academy App/Academy App/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
 
