bebfc27c8c379affe36e2f3a319a68c1a606bae2
Martin Hübelbauer <martin.hubelbauer@outlook.com>
2019-03-25 20:56:42 +0100

    implement login form, add dark keyboard variants

--- Academy App/Academy App/ViewController.swift
+++ Academy App/Academy App/ViewController.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        emailTextField.attributedPlaceholder =
+            NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(red: 128.0/255.0, green: 128.0/255.0, blue: 128.0/255.0, alpha: 1)])
+        
+        passwordTextField.attributedPlaceholder =
+            NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(red: 128.0/255.0, green: 128.0/255.0, blue: 128.0/255.0, alpha: 1)])
+        
+        emailTextField.keyboardAppearance = .dark
+        passwordTextField.keyboardAppearance = .dark
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
+    
+    
 }
 
