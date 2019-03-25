aa2c300433d5d4b6a0bee26959d4281495542e86
Martin Hübelbauer <martin.hubelbauer@outlook.com>
2019-03-25 18:17:53 +0100

    Commit xcode single view app template

---
+++ Academy App/Academy App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Academy App
+//
+//  Created by Martin Hubelbauer on 25/03/2019.
+//  Copyright © 2019 Martin Hubelbauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Academy App/Academy AppTests/Academy_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Academy_AppTests.swift
+//  Academy AppTests
+//
+//  Created by Martin Hubelbauer on 25/03/2019.
+//  Copyright © 2019 Martin Hubelbauer. All rights reserved.
+//
+
+import XCTest
+@testable import Academy_App
+
+class Academy_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
